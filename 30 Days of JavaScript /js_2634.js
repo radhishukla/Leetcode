cf8595054e3af60a587989478bd4f872bf1ffb7c
Radhika Shukla <146022535+radhishukla@users.noreply.github.com>
2025-05-18 11:24:16 +0530

    Create js_2634.js

---
+++ 30 Days of JavaScript /js_2634.js
@@ -0,0 +1,9 @@
+function filter(arr, fn) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
